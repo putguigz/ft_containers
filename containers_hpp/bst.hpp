56de94bab10555d4384ef1675c8f4a8153659679
Guillaume PETIT <gpetit@e1r9p5.42.fr>
2022-03-23 16:47:20 +0100

    map secondary fnctons, starting to to implement BST

---
+++ containers_hpp/bst.hpp
@@ -0,0 +1,16 @@
+#ifndef __BST_HPP__
+# define __BST_HPP__
+
+namespace ft{
+
+template <>
+struct BST
+{
+    value_type	elem;
+	BST*	left;
+	BST*	right;
+
+	
+};
+
+}
--- containers_hpp/map.hpp
+++ containers_hpp/map.hpp
@@ -1,15 +1,56 @@
 #ifndef __MAP_HPP__
 # define __MAP_HPP__
 
+# include "utils.hpp"
+# include "bst.hpp"
+
 namespace ft{
 
-template < class Key,                                     // map::key_type
-            class T,                                       // map::mapped_type
-            class Compare = less<Key>,                     // map::key_compare
-            class Alloc = allocator<ft::pair<const Key,T> >    // map::allocator_type
+template < class Key,
+            class T,
+            class Compare = ft::less< Key >,
+            class Alloc = allocator< ft::pair<const Key,T> >
             >
-class map{
-    typedef 
+class map
+{
+    public:
+        typedef Key                                             key_type;
+        typedef T                                               mapped_type;
+        typedef ft::pair< const Key, T >                        value_type;
+        typedef Compare                                         value_compare;
+        typedef Alloc                                           allocator_type;
+        typedef typename allocator_type::reference              reference;
+        typedef typename allocator_type::const_reference        const_reference;
+        typedef typename allocator_type::pointer                pointer;
+        typedef typename allocator_type::const_pointer          const_pointer;
+        // typedef ft::MapIterator<ft::map<T, A>, false >          iterator;
+        // typedef ft::MapIterator<ft::map<T, A>, true >           const_iterator;
+        // typedef ft::reverse_iterator<iterator>                  reverse_iterator;
+        // typedef ft::reverse_iterator<const_iterator>            const_reverse_iterator;
+        // typedef ft::iterator_traits<iterator>::difference_type  difference_type;
+        typedef A::size_type                                    size_type;
+    
+    private:
+        value_compare   _compare;
+        allocator_type  _allocker;
+        size_type       _size;
+     
+
+    public:
+        explicit map (const key_compare& comp = key_compare(), 
+                const allocator_type& alloc = allocator_type()) : _compare(comp), _allocker(alloc) { }
+        
+        map (const map& x) : _compare(x._compare), _allocker(x._allocker) { /*ADD COPY*/ }
+
+        template <class InputIterator>
+        map (InputIterator first, InputIterator last,
+        const key_compare& comp = key_compare(),
+        const allocator_type& alloc = allocator_type()) :  _compare(comp), _allocker(alloc) { /*DO THINGS*/ }
+
+        ~map( void ) { /* DESTROY AND DEALLOCATE ELEMS */ }
+
+        map&    operator=(const map& x) { /*Depends ON IMPLEMENTATION + SIZE etc...*/ }
+        allocator_type get_allocator( void ) const { return _allocker; }
 };
 
 
