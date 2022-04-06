d03c0e734de7364db040d64e06d45bded1c954a2
Guillaume PETIT <gpetit@e3r5p7.clusters.42paris.fr>
2022-04-06 18:01:44 +0200

    changed archi of traits and pairs

--- containers_hpp/utils/traits.hpp
+++ containers_hpp/utils/traits.hpp
@@ -88,50 +88,6 @@ struct is_integral<unsigned int> : public true_type{};
 template<>
 struct is_integral<unsigned long int> : public true_type{};
 
-template <class T1, class T2>
-struct pair{
-	typedef T1 first_type;
-	typedef T2 second_type;
-
-	first_type first;
-	second_type second;
-
-	pair( void ) : first(first_type()), second(second_type()){};
-	pair (const first_type& a, const second_type& b) : first(a), second(b) { };
-	~pair( void ) { };
-
-	template<class U, class V> 
-	pair (const pair<U,V>& pr) : first(pr.first), second(pr.second) { };
-
-	pair & operator=( const pair & pr)
-	{
-		if (this != &pr)
-		{
-			first = pr.first;
-			second = pr.second;
-		}
-		return *this;
-	}
-};
-
-template <class T1, class T2>
-bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return lhs.first==rhs.first && lhs.second==rhs.second; }
-
-template <class T1, class T2>
-bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return !(lhs == rhs); }
-	
-template <class T1, class T2>
-bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second);  }
-
-template <class T1, class T2>
-bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return !(rhs<lhs); }
-
-template <class T1, class T2>
-bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return rhs<lhs; }
-
-template <class T1, class T2>
-bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) { return !(lhs<rhs); }
-
 }
 
 #endif
